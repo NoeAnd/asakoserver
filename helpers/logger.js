31dec63782717861bcc1a19eb60e5846e9099142
Noe Andres <noe.andres@alea.com>
2018-02-20 16:07:46 +0100

    create logs folder if needed

--- helpers/logger.js
+++ helpers/logger.js
@@ -1,15 +1,30 @@
+//External Modules
 const fs = require('fs');
+
+//Global constants
+const LOGS_FORLDER_NAME = 'logs';
+const LOGS_DEV_NAME     = 'dev';
 
 
 
 exports.addLog = function (message) {
-    const file = 'dev';
     message = '[' + new Date() + ']' + message + '\n';
-    fs.appendFile("logs/" + file + ".txt", message, (error) => {
+
+    createFolder();
+
+
+    fs.appendFile(LOGS_FORLDER_NAME + "/" + LOGS_DEV_NAME + ".txt", message, function (error) {
         if(error) {
             throw error;
         } else {
             return true;
         }
     });
-}
+};
+
+var createFolder = function () {
+    if (!fs.existsSync('./' + LOGS_FORLDER_NAME)){
+        fs.mkdirSync(LOGS_FORLDER_NAME);
+    }
+};
+
